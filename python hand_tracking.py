0606976bcdca163022c7114244b487a9784e37a2
DatTrickster <108807317+DatTrickster@users.noreply.github.com>
2023-08-01 13:32:29 +0200

    First commit

---
+++ python hand_tracking.py
@@ -0,0 +1,54 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+def track_hands_and_control_mouse():
+    cap = cv2.VideoCapture(0)
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_draw = mp.solutions.drawing_utils
+
+    # Variables to store the previous hand coordinates
+    prev_x, prev_y = 0, 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert the frame to RGB for MediaPipe
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame to detect hands
+        results = hands.process(rgb_frame)
+
+        # Check if any hands were detected
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                # Draw landmarks on the frame
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+                # Get the coordinates of the index finger tip
+                index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                x, y = int(index_finger_tip.x * frame.shape[1]), int(index_finger_tip.y * frame.shape[0])
+
+                # Move the mouse based on hand movement
+                if prev_x != 0 and prev_y != 0:
+                    dx, dy = x - prev_x, y - prev_y
+                    pyautogui.moveRel(dx * 3, dy * 3)
+
+                prev_x, prev_y = x, y
+
+        # Display the frame
+        cv2.imshow("Hand Tracking", frame)
+
+        # Exit the loop when 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    track_hands_and_control_mouse()
+
